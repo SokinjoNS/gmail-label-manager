7cb4b4f36f81838fa9a1f64165e5155fca96839a
Sokinjo <160136911+SokinjoNS@users.noreply.github.com>
2024-03-11 17:18:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gmail-label-manager",
-    version="0.1.1",
+    version="0.1.2",
     author="SokinjoNS",
     author_email="sokinjo.155@gmail.com",
     description="A module for managing Gmail labels.",
@@ -11,7 +11,7 @@ setup(
     project_urls":{"Source":"https://github.com/SokinjoNS/gmail-label-manager"},
     packages=find_packages(),
     install_requires=[
-        'gmail-api-auth>=0.1.1',
+        'gmail-api-auth>=0.1.2',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
